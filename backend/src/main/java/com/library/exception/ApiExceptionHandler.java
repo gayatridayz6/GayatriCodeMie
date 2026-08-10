9f6385984cb953ec3ab01fdcc5b75dead311f524
gayatridayz6 <gayatri_mungarwadi@epam.com>
2026-08-10 20:35:01 +0530

    feat(EPMCDMETST-59124): add global exception handler for 400 responses

---
+++ backend/src/main/java/com/library/exception/ApiExceptionHandler.java
@@ -0,0 +1,19 @@
+package com.library.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Map;
+
+@RestControllerAdvice
+public class ApiExceptionHandler {
+
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<Map<String, Object>> handleBadRequest(BadRequestException ex) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(Map.of("error", ex.getMessage()));
+    }
+}
